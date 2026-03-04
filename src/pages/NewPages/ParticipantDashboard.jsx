ea0e89e91b403ce3ee80d10b94dbfdcc843c7de2
Rajinikanth <rajinikanthreddymadem@email.com>
2026-03-04 19:13:00 +0530

    updated dependencies

--- src/pages/NewPages/ParticipantDashboard.jsx
+++ src/pages/NewPages/ParticipantDashboard.jsx
@@ -54,7 +54,6 @@ export default function ParticipantDashboard({
     const formData = new FormData();
     formData.append('file', file);
     formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
-    formData.append('cloud_name', CLOUDINARY_CLOUD_NAME);
 
     const response = await fetch(
       `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/${resourceType}/upload`,
--- src/pages/NewPages/RegisterPage.jsx
+++ src/pages/NewPages/RegisterPage.jsx
@@ -28,7 +28,6 @@ export default function RegisterPage({ onRegister, onNavigate }) {
     const data = new FormData();
     data.append('file', file);
     data.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
-    data.append('cloud_name', CLOUDINARY_CLOUD_NAME);
     const response = await fetch(
       `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/auto/upload`,
       {
